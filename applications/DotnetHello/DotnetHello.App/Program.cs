a450be76f688791d9b0165932d75632d80176476
Zuzana Urbanová <zuzana.urbanova@qaware.de>
2022-05-23 19:02:16 +0200

    WIP: example app

---
+++ applications/DotnetHello/DotnetHello.App/Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouter(routeBuilder =>
+{
+    routeBuilder.MapGet("", context =>
+    {
+        context.Response.Redirect("index.html", permanent: false);
+        return Task.FromResult(0);
+    });
+});
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/sum", (int? n1, int? n2) => n1 + n2);
+
+app.Run();
---
+++ applications/DotnetHello/DotnetHello.Test/UnitTest.cs
@@ -0,0 +1,16 @@
+namespace DotnetHello.Test;
+
+[TestClass]
+public class UnitTest
+{
+    [TestMethod]
+    public void TestSum()
+    {
+        
+    }
+
+    [TestMethod]
+    public void TestRedirect()
+    {
+    }
+}
