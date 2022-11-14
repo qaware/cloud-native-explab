23ecd9701e55c10a30c8dae4ed82528bac3713dc
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2022-11-14 14:18:10 +0100

    Added GKE + Flux example.

--- terraform/gke-cluster-flux2/variables.tf
+++ terraform/gke-cluster-flux2/variables.tf
@@ -1,6 +1,43 @@
 variable "project_id" {
-  description = "project id"
+  description = "GCP project id"
   type        = string
+  default     = "cloud-native-experience-lab"
+}
+
+variable "region" {
+  description = "GCP region"
+  type        = string
+  default     = "europe-west1"
+}
+
+variable "gke_cluster_name" {
+  default     = "tf-flux2-cluster"
+  description = "GKE cluster name"
+  type        = string
+}
+
+variable "gke_min_nodes" {
+  default     = 3
+  description = "Minimum number of GKE nodes"
+  type        = number
+}
+
+variable "gke_max_nodes" {
+  default     = 3
+  description = "Maximum number of GKE nodes"
+  type        = number
+}
+
+variable "gke_machine_type" {
+  default     = "e2-medium"
+  description = "Machine type of GKE nodes"
+  type        = string
+}
+
+variable "preemptible" {
+  default     = false
+  description = "Preemptible GKE nodes"
+  type        = bool
 }
 
 variable "github_token" {
@@ -10,11 +47,19 @@ variable "github_token" {
 
 variable "github_owner" {
   description = "github owner"
+  default     = "qaware"
   type        = string
+}
+
+variable "github_deploy_key_title" {
+  type        = string
+  default     = "tf-flux2-cluster"
+  description = "Name of github deploy key"
 }
 
 variable "repository_name" {
   description = "repository name"
+  default     = "cloud-native-explab"
   type        = string
 }
 
@@ -26,6 +71,7 @@ variable "branch" {
 
 variable "target_path" {
   type        = string
+  default     = "clusters/gcp/tf-flux2-cluster"
   description = "Relative path to the Git repository root where the sync manifests are committed."
 }
 
@@ -33,25 +79,4 @@ variable "flux_namespace" {
   type        = string
   default     = "flux-system"
   description = "the flux namespace"
-}
-
-variable "cluster_name" {
-  type        = string
-  description = "cluster name"
-}
-
-variable "cluster_region" {
-  type        = string
-  description = "cluster region"
-}
-
-variable "use_private_endpoint" {
-  type        = bool
-  description = "Connect on the private GKE cluster endpoint"
-  default     = false
-}
-
-variable "github_deploy_key_title" {
-  type        = string
-  description = "Name of github deploy key"
 }
